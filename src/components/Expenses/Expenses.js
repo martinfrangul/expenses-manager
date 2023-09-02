4dfd9a631105b8fe3f5844a699f77226d5d94435
Martín Frangul <postguerra@gmail.com>
2023-09-02 15:43:55 +0200

    Filter agregado

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,21 +5,29 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 
 function Expenses(props) {
-  const [filteredSelection, setFilteredSelection] = useState("2020");
+  const [filteredSelection, setFilteredSelection] = useState("noFilter");
   const Selected = (select) => {
     setFilteredSelection(select);
   };
+  
+  let filteredData = props.data.filter((expense) => expense.date.getFullYear().toString() === filteredSelection)
+  if (filteredSelection === 'noFilter') {
+    filteredData = props.data
+  }
 
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredSelection} onSelected={Selected} />
-      {props.data.map((expense) => (
+      <ExpensesFilter selected={filteredSelection} onSelected={Selected} 
+      />
+      {filteredData.map((expense) => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
         />
       ))}
+      
     </Card>
   );
 }
